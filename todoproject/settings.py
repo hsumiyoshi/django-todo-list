5133b356af67146949e4b4e8e61d48389e4a4cd2
h.sumiyoshi <ha.sumiyoshi@gmail.com>
2021-06-20 09:08:25 +0900

    SECRET_KEY変更

--- todoproject/settings.py
+++ todoproject/settings.py
@@ -20,7 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g^jk--pw^!*s34w9=!9z67qqil0cmtbq2ql4ku+xj2vj65kohg# SECURITY WARNING: don't run with debug turned on in production!
+try:
+    from .local_settings import *
+except ImportError:
+    pass:
+
+# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
